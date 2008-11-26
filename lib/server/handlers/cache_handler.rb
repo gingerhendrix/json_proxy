7d7be1ec5b536511086863e61e98faefb0ba33ed
Gareth Andrew <gingerhendrix@hotmail.com>
2008-11-26 19:38:14 +0000

    Refactored cache handler

--- lib/server/handlers/cache_handler.rb
+++ lib/server/handlers/cache_handler.rb
@@ -3,53 +3,50 @@ module Server
     class CacheHandler < Handler
     
       def action(request, response, &block)
-        cache = ::Utils::CouchCache.new "#{@namespace.downcase}_#{@name.downcase}"
+        @cache = ::Utils::CouchCache.new "#{@namespace.downcase}_#{@name.downcase}"
         raise "Invalid cache_key: should be a Proc" unless @options[:cache_key] && @options[:cache_key].is_a?(Proc)
-        key = @options[:cache_key].call(*request.args)
-        result = cache.fetch key
-        if result.nil? 
-          #puts "Cache Miss #{@namespace.downcase}_#{@name.downcase}  #{key} \n"
-          yield request, response
-          if request.force?
-            cacheObj = Hash.new      
-            cacheObj['mtime'] = Time.new.to_i
-            cacheObj['ctime'] = Time.new.to_i
-            cacheObj['app_version'] = JsonProxy::APP_VERSION
-            cacheObj['data'] = response.body
-            #puts "Forced query - Storing result #{cacheObj.to_json}\n"
-            cache.store key, cacheObj.to_json
-          end
-        else
-          #puts "Result #{result}\n"
-          result =  JSON.parse result
-          if expired?(result)
-            #puts "Cache Expired:  mtime: #{result['mtime']} version: #{result['app_version']} \n"
-            yield request, response
-            if request.force?      
-              cacheObj = Hash.new      
-              cacheObj['ctime'] = result['ctime']
-              cacheObj['mtime'] = Time.new.to_i
-              cacheObj['_rev'] = result['_rev']
-              cacheObj['_id'] = result['_id']
-              cacheObj['app_version'] = JsonProxy::APP_VERSION
-              cacheObj['data'] = response.body
-              #puts "Forced query - Storing result #{cacheObj.to_json}\n"
-              cache.store key, cacheObj.to_json
-            end
+        @key = @options[:cache_key].call(*request.args)
 
+        result = @cache.fetch @key
+
+        if result.nil? 
+          yield request, response
+          add_to_cache response if request.force?
+        else
+          result =  JSON.parse result
+
+          if expired?(result)
+            yield request, response
+
+            if request.force?      
+              update_cache(result, response)
+            end
           else
-            #puts "Cache Hit #{@namespace.downcase}_#{@name.downcase}  #{key} \n"
             response.body = result['data']  
           end
         end
       end
       
-      def update_cache(key, cacheObj, response)
-      
+      def update_cache(result, response)
+        cacheObj = Hash.new      
+        cacheObj['ctime'] = result['ctime']
+        cacheObj['mtime'] = Time.new.to_i
+        cacheObj['_rev'] = result['_rev']
+        cacheObj['_id'] = result['_id']
+        cacheObj['app_version'] = JsonProxy::APP_VERSION
+        cacheObj['data'] = response.body
+        #puts "Forced query - Storing result #{cacheObj.to_json}\n"
+        @cache.store @key, cacheObj.to_json
       end
       
-      def add_to_cache(key, cacheObj, response)
-      
+      def add_to_cache(response)
+        cacheObj = Hash.new      
+        cacheObj['mtime'] = Time.new.to_i
+        cacheObj['ctime'] = Time.new.to_i
+        cacheObj['app_version'] = JsonProxy::APP_VERSION
+        cacheObj['data'] = response.body
+        #puts "Forced query - Storing result #{cacheObj.to_json}\n"
+        @cache.store @key, cacheObj.to_json      
       end
       
       def remove_from_cache(key)
