14208e50a17c5ae4bcf7b7e02901fd1bbfa8b3cb
Gareth Andrew <gingerhendrix@hotmail.com>
2009-05-06 17:54:39 +0100

    Removed puts from route_handler

--- lib/server/route_manager.rb
+++ lib/server/route_manager.rb
@@ -6,20 +6,22 @@ module Server
     
     def initialize
       @map = Hash.new
+      @log = Logging.logger[self]
     end
        
     def route_for(path)
       namespace = path.slice /\/(.*)\/(.*)\.(.*)$/, 1
       name = path.slice /\/(.*)\/(.*)\.(.*)$/, 2
-      puts "Namespace: #{namespace}  name: #{name}  map: #{@map} \n"  
+      @log.info "Namespace: #{namespace}  name: #{name}  map: #{@map}"  
       if (@map.key? namespace)
-        puts "Namespace found\n"
+        @log.info "Namespace found"
         if (@map[namespace].key? name)
-            puts "Name found\n"
-            @map[namespace][name]
+          @log.info "Name found"
+          return @map[namespace][name]
         end
       else
-        puts "Namespace not found"
+        @log.info "Namespace not found"
+        return nil
       end
     end
 
@@ -28,7 +30,7 @@ module Server
         @map[namespace] = Hash.new
       end
       @map[namespace][name] = route
-      puts "Registered #{namespace}/#{name}\n"
+      @log.info "Registered #{namespace}/#{name}\n"
     end
 
   end
